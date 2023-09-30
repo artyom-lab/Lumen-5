19d357163eb812b65e1e3f0ff6b63970d5669403
artyom-lab <artyomgalkeane@gmail.com>
2023-09-30 21:47:56 +0300

    689

--- js/common.js
+++ js/common.js
@@ -33,6 +33,10 @@ $(".close-metrics").on("click", function(){
   $(this).closest(".metric-alert").hide();
 });
 
+$(".close-box").on("click", function(){
+  $(this).closest(".pitch-box").hide();
+});
+
 $('#sidebar-switcher').on('change', function() {
   if ($(this).is(':checked')) 
     $('.sidebar-2').addClass('sidebar-absolute');
@@ -112,26 +116,26 @@ $(".news-select").on("click", function(){
   $(".row-hidden").show();
 });
 
-  $("#v-pills-store-tab").on("click", function(){
+  $("#v-pills-first-tab").on("click", function(){
     $(this).removeClass("done");
-    $("#v-pills-quote-tab, #v-pills-monthly-tab, #v-pills-review-tab").removeClass("done");
+    $("#v-pills-second-tab, #v-pills-third-tab, #v-pills-fourth-tab").removeClass("done");
   });
 
-  $("#v-pills-quote-tab").on("click", function(){
+  $("#v-pills-second-tab").on("click", function(){
     $(this).removeClass("done");
-    $("#v-pills-store-tab").addClass("done");
-    $("#v-pills-monthly-tab, #v-pills-review-tab").removeClass("done");
+    $("#v-pills-first-tab").addClass("done");
+    $("#v-pills-third-tab, #v-pills-fourth-tab").removeClass("done");
   });
 
-  $("#v-pills-monthly-tab").on("click", function(){
+  $("#v-pills-third-tab").on("click", function(){
     $(this).removeClass("done");
-    $("#v-pills-store-tab, #v-pills-quote-tab").addClass("done");
-    $("#v-pills-review-tab").removeClass("done");
+    $("#v-pills-first-tab, #v-pills-second-tab").addClass("done");
+    $("#v-pills-fourth-tab").removeClass("done");
   });
 
-  $("#v-pills-review-tab").on("click", function(){
+  $("#v-pills-fourth-tab").on("click", function(){
     $(this).removeClass("done");
-    $("#v-pills-store-tab, #v-pills-quote-tab, #v-pills-monthly-tab").addClass("done");
+    $("#v-pills-first-tab, #v-pills-second-tab, #v-pills-third-tab").addClass("done");
   });
 
 // SELECT
@@ -185,6 +189,83 @@ $('input[name="birthday1"]').daterangepicker({
   minYear: 2000,
   maxYear: parseInt(moment().format('YYYY'),10)
 });
+
+  // SELECT2
+
+    $("select.select2").select2({
+    allowClear: true
+  });
+
+// (function($) {
+//   var Defaults = $.fn.select2.amd.require('select2/defaults');
+//   $.extend(Defaults.defaults, {
+//     dropdownPosition: 'auto'
+//   });
+//   var AttachBody = $.fn.select2.amd.require('select2/dropdown/attachBody');
+//   var _positionDropdown = AttachBody.prototype._positionDropdown;
+//   AttachBody.prototype._positionDropdown = function() {
+//     var $window = $(window);
+//     var isCurrentlyAbove = this.$dropdown.hasClass('select2-dropdown--above');
+//     var isCurrentlyBelow = this.$dropdown.hasClass('select2-dropdown--below');
+//     var newDirection = null;
+//     var offset = this.$container.offset();
+//     offset.bottom = offset.top + this.$container.outerHeight(false);
+//     var container = {
+//         height: this.$container.outerHeight(false)
+//     };
+//     container.top = offset.top;
+//     container.bottom = offset.top + container.height;
+//     var dropdown = {
+//       height: this.$dropdown.outerHeight(false)
+//     };
+//     var viewport = {
+//       top: $window.scrollTop(),
+//       bottom: $window.scrollTop() + $window.height()
+//     };
+//     var enoughRoomAbove = viewport.top < (offset.top - dropdown.height);
+//     var enoughRoomBelow = viewport.bottom > (offset.bottom + dropdown.height);
+//     var css = {
+//       left: offset.left,
+//       top: container.bottom
+//     };
+//     // Determine what the parent element is to use for calciulating the offset
+//     var $offsetParent = this.$dropdownParent;
+//     // For statically positoned elements, we need to get the element
+//     // that is determining the offset
+//     if ($offsetParent.css('position') === 'static') {
+//       $offsetParent = $offsetParent.offsetParent();
+//     }
+//     var parentOffset = $offsetParent.offset();
+//     css.top -= parentOffset.top
+//     css.left -= parentOffset.left;
+//     var dropdownPositionOption = this.options.get('dropdownPosition');
+//     if (dropdownPositionOption === 'above' || dropdownPositionOption === 'below') {
+//       newDirection = dropdownPositionOption;
+//     } else {
+//       if (!isCurrentlyAbove && !isCurrentlyBelow) {
+//         newDirection = 'below';
+//       }
+//       if (!enoughRoomBelow && enoughRoomAbove && !isCurrentlyAbove) {
+//         newDirection = 'above';
+//       } else if (!enoughRoomAbove && enoughRoomBelow && isCurrentlyAbove) {
+//         newDirection = 'below';
+//       }
+//     }
+//     if (newDirection == 'above' ||
+//     (isCurrentlyAbove && newDirection !== 'below')) {
+//         css.top = container.top - parentOffset.top - dropdown.height;
+//     }
+//     if (newDirection != null) {
+//       this.$dropdown
+//         .removeClass('select2-dropdown--below select2-dropdown--above')
+//         .addClass('select2-dropdown--' + newDirection);
+//       this.$container
+//         .removeClass('select2-container--below select2-container--above')
+//         .addClass('select2-container--' + newDirection);
+//     }
+//     this.$dropdownContainer.css(css);
+//   };
+// })(window.jQuery);
 
 });
 
@@ -255,4 +336,6 @@ function updateThumbnail(dropZoneElement, file) {
     thumbnailElement.style.backgroundImage = null;
   }
 }
+
+
 
